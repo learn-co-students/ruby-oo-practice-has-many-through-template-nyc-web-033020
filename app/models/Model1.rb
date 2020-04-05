c9bf78a18960887ab968d37834148727c1b20076
mishyjari <mishyjari@outlook.com>
2020-04-05 14:52:56 -0400

    boilerplate initial

--- app/models/Model1.rb
+++ app/models/Model1.rb
@@ -1,5 +1,17 @@
 #class for Model1 goes here
 #Feel free to change the name of the class
 class Model1
+  @all = []
+  
+  attr_reader
+  attr_accessor
 
+  def initialize
+
+    Model.all << self
+  end
+
+  def self.all 
+    @@all
+  end
 end
--- app/models/Model2.rb
+++ app/models/Model2.rb
@@ -1,5 +1,17 @@
 #class for Model2 goes here
 #Feel free to change the name of the class
 class Model2
+  @all = []
+  
+  attr_reader
+  attr_accessor
+
+  def initialize
+
+    Model.all << self
+  end
+
+  def self.all 
+    @@all
 
 end
